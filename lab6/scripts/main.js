889530cf6ce268eaaa0fd2aa0aaf3bc9a7f30807
andre-marques-a22207598 <127451848+andre-marques-a22207598@users.noreply.github.com>
2025-10-28 16:57:08 +0000

    inicio lab6

---
+++ lab6/scripts/main.js
@@ -0,0 +1,8 @@
+import { produtos } from './produtos.js'
+
+const produtos_section = document.querySelector('#produtos');
+const cesto_section = document.querySelector('#cesto');
+const quantProdutos_p = document.querySelector('#quantidadeProdutos');
+const escolherProdutos = document.querySelector('#escolherProdutos');
+let quantProduto = 0;
+
